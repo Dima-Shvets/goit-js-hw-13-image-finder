aa273acecbb700c7b3804893a2fdc0d18a9a05ea
Dima-Shvets <81581083+Dima-Shvets@users.noreply.github.com>
2021-10-17 09:58:26 +0300

    adds templates

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './sass/main.scss';
+
+// const BASE_URL = 'https://pixabay.com/api'
+
+// async function fetchImages() {
+//     const responce = await fetch(`${BASE_URL}/?image_type=photo&orientation=horizontal&q=cats&key=23893693-5116f7539f40f5f078cda9a9c`)
+//     return await responce.json();
+// }
+
+
+
+
+
