e8e4e8583e9f7dabffefcb95567cd2b6b82e74b8
Dima-Shvets <81581083+Dima-Shvets@users.noreply.github.com>
2021-10-18 21:12:19 +0300

    adds cards markup and load more btn

--- src/index.js
+++ src/index.js
@@ -1,11 +1,49 @@
 import './sass/main.scss';
 
-// const BASE_URL = 'https://pixabay.com/api'
+import debounce from "lodash.debounce";
 
-// async function fetchImages() {
-//     const responce = await fetch(`${BASE_URL}/?image_type=photo&orientation=horizontal&q=cats&key=23893693-5116f7539f40f5f078cda9a9c`)
-//     return await responce.json();
-// }
+import galleryTpl from './templates/gallery-tpl.hbs';
+import photoCardTpl from './templates/photo-card-tpl.hbs';
+import searchFormTpl from './templates/search-form-tpl.hbs';
+
+const BASE_URL = 'https://pixabay.com/api';
+const pageNumber = 1;
+const currentQuery = ""
+
+const body = document.querySelector('body');
+
+body.insertAdjacentHTML('afterbegin', galleryTpl());
+body.insertAdjacentHTML('afterbegin', searchFormTpl());
+
+const refs = {
+    searchForm: document.querySelector('.search-form'),
+    gallery: document.querySelector('.gallery'),
+    loadMoreBtn: document.querySelector('[data-action="load-more-btn"]')
+}
+
+refs.searchForm.query.addEventListener('input', debounce(onFormInputChange, 500));
+refs.loadMoreBtn.addEventListener('click', onLoadBtnPress());
+
+
+function fetchPictures(query) {
+    return fetch(`${BASE_URL}/?image_type=photo&orientation=horizontal&q=${query}&page=${pageNumber}&per_page=12&key=23893693-5116f7539f40f5f078cda9a9c`)
+        .then(responce => responce.json()).then(pictures => {
+            const cardsMarkup = photoCardTpl(pictures.hits);
+            refs.gallery.innerHTML = cardsMarkup;
+            pageNumber += 1;
+    });
+}
+
+function onFormInputChange(e) {
+    const currentQuery = e.target.value;
+    fetchPictures(currentQuery);
+    pageNumber = 1;
+}
+
+function onLoadBtnPress() {
+    fetchPictures(currentQuery);
+}
+
 
 
 
