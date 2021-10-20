fa303855625ec3128ba3e51e83b210c97b32106a
Dima-Shvets <81581083+Dima-Shvets@users.noreply.github.com>
2021-10-20 21:11:00 +0300

    corrects mistakes

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import photoCardTpl from './templates/photo-card-tpl.hbs';
 import searchFormTpl from './templates/search-form-tpl.hbs';
 
 const BASE_URL = 'https://pixabay.com/api';
-const pageNumber = 1;
-const currentQuery = ""
+let pageNumber = 1;
+const currentQuery = '';
 
 const body = document.querySelector('body');
 
@@ -25,23 +25,36 @@ refs.searchForm.query.addEventListener('input', debounce(onFormInputChange, 500)
 refs.loadMoreBtn.addEventListener('click', onLoadBtnPress());
 
 
-function fetchPictures(query) {
-    return fetch(`${BASE_URL}/?image_type=photo&orientation=horizontal&q=${query}&page=${pageNumber}&per_page=12&key=23893693-5116f7539f40f5f078cda9a9c`)
+function fetchPictures(query, pageNmbr) {
+    return fetch(`${BASE_URL}/?image_type=photo&orientation=horizontal&q=${query}&page=${pageNmbr}&per_page=12&key=23893693-5116f7539f40f5f078cda9a9c`)
         .then(responce => responce.json()).then(pictures => {
-            const cardsMarkup = photoCardTpl(pictures.hits);
-            refs.gallery.innerHTML = cardsMarkup;
-            pageNumber += 1;
+            appendCardsMarkup(pictures.hits)
+            incrementPage();
     });
 }
 
+function appendCardsMarkup(pictures) {
+    refs.gallery.insertAdjacentHTML('beforeend', photoCardTpl(pictures))
+}
+
 function onFormInputChange(e) {
+
     const currentQuery = e.target.value;
-    fetchPictures(currentQuery);
+    fetchPictures(currentQuery, pageNumber);
+    refs.gallery.innerHTML = "";
     pageNumber = 1;
 }
 
 function onLoadBtnPress() {
-    fetchPictures(currentQuery);
+    console.log('button')
+     if (currentQuery.length === 0) {
+        return
+    };
+    fetchPictures(currentQuery, pageNumber);
+}
+
+function incrementPage() {
+    pageNumber += 1;
 }
 
 
